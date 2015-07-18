b6c8cc49895d51831ff021e871a4755144ae5ac4
guttikar <guttikar@users.noreply.github.com>
2015-07-18 22:49:42 +0530

    gh-pages first commit

---
+++ index-content.php
@@ -0,0 +1,84 @@
+ <?php $page_name = 'index-content'; ?>
+      <div class="cover-slider">
+       <div class="slide1">
+          <div class="intro-text large-12 columns">
+            <p>Lorem ipsum.<br>Lorem ipsum dolor.</p>
+          </div>
+        </div>
+        <div class="slide2">
+          <div class="intro-text large-12 columns">
+            <p>Praesentium, corrupti!<br>Commodi libero, velit.</p>
+          </div>
+        </div>
+        <div class="slide3">
+          <div class="intro-text large-12 columns">
+            <p>Ad, hic.<br>Laboriosam ad, sit.</p>
+          </div>
+        </div>
+        <div class="slide4">
+          <div class="intro-text large-12 columns">
+            <p>Quae, nisi.<br>Doloremque, sunt, animi.</p>
+          </div>
+        </div>
+        <div class="slide5">
+          <div class="intro-text large-12 columns">
+            <p>Vel, dolorum!<br>Officia, veritatis, vel.</p>
+          </div>
+        </div>
+       </div>
+      
+      <div class="full-width dedede">
+        <div class="row about">
+          <div class="small-centered  medium-uncentered medium-5 large-4 columns">
+            <img src="http://placehold.it/600x600">
+          </div>
+          <div class="medium-7 large-8 columns">
+            <h1>About Karwak</h1>
+            <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+          </div>
+            <ul class="small-block-grid-3 medium-12 large-8 columns">
+              <h3 class="recycled">100% recycled and environment friendly</h1>
+              <li>
+                <h4>paper</h4>
+                <p class=""> - Lorem</br>- ipsum dolor</br>- sit amet</p>
+                <div class="tiny button">buy product</div>
+              </li>
+              <li>
+                <h4>wood</h4>
+                <p class=""> - Nulla</br>- ham qui</br>- sint exercitation</p>
+                <div class="button tiny">buy product</div>
+              </li>
+              <li>
+                <h4>bamboo</h4>
+                <p class=""> - Eiusmod</br>- swine spare</br>- boudin aliqua</p>
+                <div class="tiny button">buy product</div>
+              </li>
+            </ul>
+        </div>
+      </div>
+    
+      <div class="row process">
+        <h1>how products are made at karwak</h1>
+        <div class="panel"></div>
+      </div>
+    
+      <div class="row changing">
+        <h1>By using Karwak products these many things are changing</h1>
+        <div class="panel"></div>
+      </div>
+      <div class="full-width haldi" style="min-height: 6rem;">&nbsp;</div>
+      <div class="full-width dedede">
+        <div class="row">
+          <span class="strong uppercase">What clients say!</span>
+        </div>
+      </div>
+      <div class="full-width">
+        <div class="row testimonial">
+          <div class="testimonial-slider small-centered medium-10 medium-offset-1 columns">
+            <div class=""><img src="../pngs/nike.png" alt=""></div>
+            <div class=""><p style="width: 37%; margin: auto 0 auto 1em; padding-top: 1em">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni architecto, quibusdam consequatur officia ullam aperiam adipisci odit iste commodi eos illo saepe doloremque alias quam, neque reprehenderit cum excepturi ab!</p></div>
+            <div class=""><img src="../pngs/new-balance.png" alt=""></div>
+            <div class=""><p style="width:35%; margin: auto 0 auto 1em;padding-top: 2em; overflow: visible !important;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni architecto, quibusdam consequatur officia ullam aperiam adipisci odit iste commodi eos illo saepe doloremque alias quam, neque reprehenderit cum excepturi ab!</p></div>
+          </div>
+        </div>
+      </div>     
